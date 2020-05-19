37f37ef6a3d1ee290b689bde610810fa5ef4e87e
Perera <per24h@csiro.au>
2020-05-19 16:10:38 +1000

    Aluminum concentrations fixed in the data and end of tidy data

--- script.R
+++ script.R
@@ -4,23 +4,23 @@ library(readxl)
 #tidying up data
 
 long_root <- read_excel("data/experiment_39.xlsx", sheet="long_root") %>% 
-  gather(treatment,longest_root, treatment_1, treatment_2, treatment_3) %>% 
+  gather(treatment,longest_root, "0 µM", "5 µM", "10 µM") %>% 
   mutate(longest_root=as.numeric(longest_root))
 long_root 
 
 shoot_dw <- read_excel("data/experiment_39.xlsx", sheet="shoot_dw") %>% 
-  gather(treatment,shoot_dw, treatment_1, treatment_2, treatment_3) %>% 
+  gather(treatment,shoot_dw, "0 µM", "5 µM", "10 µM") %>% 
   mutate(shoot_dw=as.numeric(shoot_dw))
 
 shoot_dw
 
 root_dw <- read_excel("data/experiment_39.xlsx", sheet="root_dw") %>% 
-  gather(treatment,root_dw,treatment_1, treatment_2, treatment_3)%>% 
+  gather(treatment,root_dw,"0 µM", "5 µM", "10 µM")%>% 
   mutate(root_dw=as.numeric(root_dw))
 root_dw 
 
 root_length <- read_excel("data/experiment_39.xlsx", sheet="root_length") %>% 
-  gather(treatment,root_length,treatment_1, treatment_2, treatment_3)%>% 
+  gather(treatment,root_length,"0 µM", "5 µM", "10 µM")%>% 
   mutate(root_length=as.numeric(root_length))
 root_length
 
@@ -32,7 +32,7 @@ view(ex39_combined)
 
 ex39 <-select(ex39_combined,1,2,3,4,8,12,16) %>% 
   mutate(total_dw=shoot_dw+root_dw)%>% 
-  filter(longest_root != "-",shoot_dw != "-",root_dw != "-", root_length!= "-", total_dw!= "-")
+  filter(longest_root != "-")
 ex39
 view (ex39)
 
