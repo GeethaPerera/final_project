c72fe81722110c659163f167f3ee4dcead4b2e8e
Perera <per24h@csiro.au>
2020-05-20 14:52:58 +1000

    Tidy data for the plots

--- script.R
+++ script.R
@@ -1,27 +1,32 @@
 library(tidyverse)
 library(readxl)
+library(cowplot)
 
 #tidying up data
 
-long_root <- read_excel("data/experiment_39.xlsx", sheet="long_root") %>% 
+long_root <- read_excel("Untitled/data/experiment_39.xlsx", sheet="long_root") %>% 
   gather(treatment,longest_root, "0 µM", "5 µM", "10 µM") %>% 
   mutate(longest_root=as.numeric(longest_root))
+long_root$treatment <- factor(long_root$treatment,levels = c("0 µM", "5 µM", "10 µM"))
 long_root 
 
-shoot_dw <- read_excel("data/experiment_39.xlsx", sheet="shoot_dw") %>% 
+shoot_dw <- read_excel("Untitled/data/experiment_39.xlsx", sheet="shoot_dw") %>% 
   gather(treatment,shoot_dw, "0 µM", "5 µM", "10 µM") %>% 
   mutate(shoot_dw=as.numeric(shoot_dw))
+shoot_dw$treatment <- factor(shoot_dw$treatment,levels = c("0 µM", "5 µM", "10 µM"))
 
 shoot_dw
 
-root_dw <- read_excel("data/experiment_39.xlsx", sheet="root_dw") %>% 
+root_dw <- read_excel("Untitled/data/experiment_39.xlsx", sheet="root_dw") %>% 
   gather(treatment,root_dw,"0 µM", "5 µM", "10 µM")%>% 
   mutate(root_dw=as.numeric(root_dw))
+root_dw$treatment <- factor(root_dw$treatment,levels = c("0 µM", "5 µM", "10 µM"))
 root_dw 
 
 root_length <- read_excel("data/experiment_39.xlsx", sheet="root_length") %>% 
   gather(treatment,root_length,"0 µM", "5 µM", "10 µM")%>% 
   mutate(root_length=as.numeric(root_length))
+root_length$treatment <- factor(root_length$treatment,levels = c("0 µM", "5 µM", "10 µM"))
 root_length
 
 ex39_combined <- bind_cols(long_root,shoot_dw,root_dw,root_length) 
@@ -36,3 +41,97 @@ ex39 <-select(ex39_combined,1,2,3,4,8,12,16) %>%
 ex39
 view (ex39)
 
+# show the raw data using boxplots
+
+# length of longest root
+
+long_root<- ggplot(data=ex39, aes(x=genotype, y=longest_root, colour=treatment))+geom_boxplot()+facet_wrap(~treatment)
+
+long_root
+
+long_root_final <- long_root+
+  labs(title="length of longest root", x= "genotype", y="root length (mm)")+
+  theme_bw()+
+  theme (axis.text = element_text(size = 6, angle = 90),
+         axis.title = element_text(size = 8),
+         plot.title = element_text(size = 12),
+         legend.title = element_text(size = 10),
+         strip.text = element_text(size = 6))
+
+long_root_final
+
+# shoot dry weight
+
+sdw <- ggplot(data=ex39, aes(x=genotype, y=shoot_dw, colour=treatment))+geom_boxplot()+facet_wrap(~treatment)
+
+sdw
+
+sdw_final <- sdw+
+  labs(title="Shoot dry weight", x= "genotype", y="shoot dry weight (mg)")+
+  theme_bw()+
+  theme (axis.text = element_text(size = 6, angle = 90),
+         axis.title = element_text(size = 8),
+         plot.title = element_text(size = 12),
+         legend.title = element_text(size = 10),
+         strip.text = element_text(size = 6))
+
+sdw_final
+
+# root dry weight
+
+rdw <- ggplot(data=ex39, aes(x=genotype, y=root_dw, colour=treatment))+geom_boxplot()+facet_wrap(~treatment)
+
+rdw
+rdw_final <- rdw+
+  labs(title="Root dry weight", x= "genotype", y="root dry weight (mg)")+
+  theme_bw()+
+  theme (axis.text = element_text(size = 6, angle = 90),
+         axis.title = element_text(size = 8),
+         plot.title = element_text(size = 12),
+         legend.title = element_text(size = 10),
+         strip.text = element_text(size = 6))
+
+rdw_final
+
+# total dry weight
+
+tdw <- ggplot(data=ex39, aes(x=genotype, y=total_dw, colour=treatment))+geom_boxplot()+facet_wrap(~treatment)
+
+tdw
+
+tdw_final <- tdw+
+  labs(title="Total dry weight", x= "genotype", y="total dry weight (mg)")+
+  theme_bw()+
+  theme (axis.text = element_text(size = 6, angle = 90),
+         axis.title = element_text(size = 8),
+         plot.title = element_text(size = 12),
+         legend.title = element_text(size = 10),
+         strip.text = element_text(size = 6))
+
+tdw_final
+
+# root length
+
+rt_length <- ggplot(data=ex39, aes(x=genotype, y=root_length, colour=treatment))+geom_boxplot()+facet_wrap(~treatment)
+
+rt_length
+rt_length_final <- rt_length+
+  labs(title="Total root length", x= "genotype", y="root length (cm)")+
+  theme_bw()+
+  theme (axis.text = element_text(size = 6, angle = 90),
+         axis.title = element_text(size = 8),
+         plot.title = element_text(size = 12),
+         legend.title = element_text(size = 10),
+         strip.text = element_text(size = 6))
+
+rt_length_final
+
+# final boxplot for dry weights 
+
+final_plot_dw <- plot_grid(sdw_final+theme(legend.position ="none"), rdw_final+theme(legend.position ="none"), tdw_final+theme(legend.position ="none"))
+final_plot_dw
+
+# final boxplot for root lengths 
+
+final_plot_rl <- plot_grid(long_root_final+theme(legend.position ="none"),rt_length_final+theme(legend.position ="none"))
+final_plot_rl
